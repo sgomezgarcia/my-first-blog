d67dc8276e2c7f06cf1e3d854d3d6cc655bbdb37
sgomezgarcia <saragmzgrc@gmail.com>
2019-02-16 17:56:35 +0100

    delete button

--- blog/views.py
+++ blog/views.py
@@ -21,14 +21,19 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
+    if request.method == "POST" and 'delete' in request.POST:
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post.delete()
+            return redirect('/')
+    elif request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
@@ -39,3 +44,4 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
